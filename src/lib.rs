426bfb0ce6f211b13b4fffc4ec962ff79e393a6f
I-luk-I <105889683+I-luk-I@users.noreply.github.com>
2024-08-06 08:44:59 +0300

    Add files via upload

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use elliptic_curve::rand_core::{OsRng, RngCore};
+use secp256k1::{SecretKey, PublicKey, Secp256k1};
+use sha2::{Digest, Sha256};
+use ripemd::{Ripemd160, Digest as RipemdDigest};
+use hex;
+pub trait Wallet{
+    fn new()->Self;
+    fn get_address(&self)->&str;
+    fn get_priv_key(&self)->&str;
+    fn get_public_key(&self)->&str;
+}
+pub struct Wart_key{
+    priv_key:&'static str,
+    public_key:&'static str,
+    address:&'static str
+}
+impl Wallet for Wart_key{
+    fn new()->Self{
+        let mut sk_bytes = [0u8; 32];
+        let mut rng = OsRng;
+        rng.fill_bytes(&mut sk_bytes);
+        //println!("private_key: {}", hex::encode(&sk_bytes));
+        let secp = Secp256k1::new();
+        let sk = SecretKey::from_slice(&sk_bytes)
+            .expect("Error creating secret key from slice");
+        let pub_key = PublicKey::from_secret_key(&secp, &sk);
+        //println!("public key: {}", hex::encode(pubkey.serialize()));
+        let sha = Sha256::digest(&pub_key.serialize());
+        let addr_raw = Ripemd160::digest(&sha);
+        let mut hasher_checksum = Sha256::new();
+        hasher_checksum.update(&addr_raw);
+        let checksum = &hasher_checksum.finalize()[..4];
+        let addr = [&addr_raw[..], checksum].concat();
+        //println!("address: {}", hex::encode(addr));
+        Wart_key{
+            priv_key:Box::leak((hex::encode(&sk_bytes)).into_boxed_str()),
+            public_key:Box::leak(hex::encode(pub_key.serialize()).into_boxed_str()),
+            address:Box::leak(hex::encode(addr).into_boxed_str())
+        }
+    }
+    fn get_address(&self) -> &str {
+        self.address
+    }
+    fn get_priv_key(&self) -> &str {
+        self.priv_key
+    }
+    fn get_public_key(&self) -> &str {
+        self.public_key
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,69 @@
+use std::io::{Read, stdin};
+use std::ops::Deref;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread::JoinHandle;
+use Parse_new::Wart_key;
+mod lib;
+use::Parse_new::Wallet;
+use std::thread;
+use num_cpus;
 
+fn main() {
+    let mut choice = String::new();
+    println!("Choose one of the options:\n1-Create a random address\n2-Create your desired address.");
+    stdin().read_line(&mut choice).unwrap();
+    let choice = choice.trim();
+    match choice {
+        "1"=>{
+            let address = Wart_key::new();
+            println!("Wart address - {}\nWart public key - {}\nWart private key - {}",address.get_address(),address.get_public_key(),address.get_priv_key())
+        }
+        "2"=>{
+            let mut target = String::new();
+            println!("Enter address generation criteria (end of address)");
+            stdin().read_line(&mut target).unwrap();
+            let target = Arc::new(target.trim().to_string());
+            let  found =  Arc::new(AtomicBool::new(false));
+            let mut vec_threads:Vec<JoinHandle<()>> = Vec::new();
+            let address_count = Arc::new(Mutex::new(0));
+            for i in 0..num_cpus::get(){
+                let count_adr = Arc::clone(&address_count);
+                let arc_count = Arc::clone(&target);
+                let found = Arc::clone(&found);
+                let thread = thread::spawn( move ||
+                loop  {
+                    if found.load(Ordering::Relaxed){
+                        break
+                    }
+                    let vrem_adres = Wart_key::new();
+                    match &vrem_adres.get_address()[vrem_adres.get_address().len() - arc_count.len()..] == *arc_count {
+                        false => {
+                            let mut lock_count = count_adr.lock().unwrap();
+                            *lock_count+=1;
+                            if *lock_count%100000 == 0{
+                                println!("Generated addresses:{}",*lock_count);
+
+                            }
+                        }
+                        true => {
+                            println!("Wart address - {}\nWart public key - {}\nWart private key - {}", vrem_adres.get_address(), vrem_adres.get_public_key(), vrem_adres.get_priv_key());
+                            found.store(true,Ordering::Relaxed);
+                            break
+                        }
+
+                    }
+
+
+                });
+                vec_threads.push(thread);
+            }
+            for i in vec_threads.into_iter(){
+                i.join().unwrap()
+            }
+
+        }
+        _=>println!("Wrong choice")
+    }
+
+}
