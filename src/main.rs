b7a5cd834e87de65351fc876b4a709f4b48dd8fb
I-luk-I <105889683+I-luk-I@users.noreply.github.com>
2024-10-30 14:14:23 +0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::Args;
 use std::io::{Read, stdin};
 use std::ops::Deref;
 use std::sync::{Arc};
@@ -8,31 +9,44 @@ mod lib;
 use std::thread;
 use num_cpus;
 use std::sync::atomic::{AtomicU64,AtomicBool};
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use sha2::digest::consts::True;
 use::Parse_new::Wallet;
+use clap::Parser;
+#[derive(Parser)]
+struct Threads{
+    #[arg(short,long)]
+    num_threads:u8
+}
 
 fn main() {
+    let threads = Threads::try_parse();
     let mut choice = String::new();
     println!("Choose one of the options:\n1-Create a random address\n2-Create your desired address.");
     stdin().read_line(&mut choice).unwrap();
     let choice = choice.trim();
     let global_count = Arc::new(std::sync::atomic::AtomicU64::new(0));
     let  found =  Arc::new(AtomicBool::new(false));
+    let mut vec_threads:Vec<JoinHandle<()>> = Vec::new();
+    let main_discriptor = std::thread::current();
+    let time = std::time::Instant::now();
     match choice {
         "1"=>{
             let address = Wart_key::new();
-            println!("Wart address - {}\nWart public key - {}\nWart private key - {}",address.get_address(),address.get_public_key(),address.get_priv_key())
+            println!("Wart address - {}\nWart public key - {}\nWart private key - {}",address.get_address(),address.get_public_key(),address.get_priv_key());
+            return
         }
         "2"=>{
             let mut target = String::new();
             println!("Enter address generation criteria (end of address)");
             stdin().read_line(&mut target).unwrap();
             let target = Arc::new(target.trim().to_string());
-            let mut vec_threads:Vec<JoinHandle<()>> = Vec::new();
-            for i in 0..num_cpus::get() + 2{
+            let threads = if let Ok(i) = threads {i.num_threads} else { num_cpus::get() as u8 + 2 };
+            for i in 0..threads{
                 let found_clone = Arc::clone(&found);
                 let global_count_clone = Arc::clone(&global_count);
                 let target_clone = Arc::clone(&target);
+                let main_discriptor = main_discriptor.clone();
                 let thread = thread::spawn( move ||{
                     loop  {
                         if found_clone.load(Ordering::Relaxed){
@@ -41,36 +55,33 @@ fn main() {
                         let temp_address = Wart_key::new();
                         match &temp_address.get_address()[temp_address.get_address().len() - target_clone.len()..] == *target_clone {
                             false => {
-                                global_count_clone.fetch_add(1, Ordering::SeqCst);
+                                let count = global_count_clone.fetch_add(1, Ordering::SeqCst);
+                                if count % 1000_000 == 0{
+                                    main_discriptor.unpark();
+                                }
                             }
                             true => {
                                 println!("Wart address - {}\nWart public key - {}\nWart private key - {}", temp_address.get_address(), temp_address.get_public_key(), temp_address.get_priv_key());
                                 found_clone.store(true,Ordering::Relaxed);
+                                main_discriptor.unpark();
                             }
                         }
                     }});
 
                 vec_threads.push(thread);
             }
-            let print_glob = Arc::clone(&global_count);
-            let found_clone = Arc::clone(&found);
-            let print_count = std::thread::spawn(move ||{
-                loop {
-                    if found_clone.load(Ordering::Relaxed){
-                        break
-                    }
-                    println!("Addresses generated: {}",print_glob.load(Ordering::Relaxed));
-                    thread::sleep(Duration::from_secs(15))
 
 
-                }
-
-            });
-            vec_threads.push(print_count);
-            vec_threads.into_iter().for_each(|i| i.join().unwrap() )
         }
-        _=>println!("Wrong choice")
+        _=>{println!("Wrong choice"); return}
     }
+    while !found.load(Ordering::SeqCst) {
+        let count = global_count.load(Ordering::Relaxed);
+        println!("Addresses generated: {}  speed around {} in min",count, count/time.elapsed().as_secs() * 60);
+        std::thread::park();
+    }
+
+    vec_threads.into_iter().for_each(|i| i.join().unwrap() )
 
 }
 
